4a6d8a591d52624b893beaaf454a2c09863251ef
sehwa <love7620640@naver.com>
2023-02-06 20:37:23 +0900

    0206

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        background: "white",
+        width: "560px",
+        height: "800px",
+        borderRadius: "8px",
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div>
+        <div style={{ padding: "56px 36px", textAlign: "left" }}>
+          <h1
+            style={{ fontSize: "40px", color: "#2e2e2e", marginBottom: "32px" }}
+          >
+            2023년 2월 5일
+          </h1>
+        </div>
+        {/*  */}
+        <div>
+          <input
+            style={{
+              border: "none",
+              borderBottom: "1px solid #cfcfcf",
+              width: "100%",
+            }}
+          />
+          <button>추가</button>
+        </div>
+
+        <div>
+          <ul></ul>
+
+          {/* 버튼 2개, state관리, 변수 설정 해보기, click 시 */}
+        </div>
+      </div>
     </div>
   );
 }
